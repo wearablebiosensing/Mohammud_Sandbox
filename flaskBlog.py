8e576503abccb33c0ff7fa7c5ec424284cfc9239
1moha <balizain557@gmail.com>
2022-01-31 16:44:53 -0500

    First flask app

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'data_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'data_posted': 'April 21, 2018'
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home page!</h1>"
+    #render the template
+    return render_template('home.html', posts=posts)
 
 
-
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
 
