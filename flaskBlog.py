3ad69047982aa05d4b2d8e7fea93388f82de0465
1moha <balizain557@gmail.com>
2022-02-07 11:38:29 -0500

    adding bootstrap and styling

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,36 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '40fe7d8a05985dd886eb607be360fdac'
+
 
 posts = [
     {
         'author': 'Corey Schafer',
-        'title': 'Blog post 1',
+        'title': 'Blog Post 1',
         'content': 'First post content',
-        'data_posted': 'April 20, 2018'
+        'date_posted': 'April 20, 2018'
     },
     {
         'author': 'Jane Doe',
-        'title': 'Blog post 2',
+        'title': 'Blog Post 2',
         'content': 'Second post content',
-        'data_posted': 'April 21, 2018'
-    },
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
-@app.route('/')
-@app.route('/home')
+
+@app.route("/")
+@app.route("/home")
 def home():
-    #render the template
     return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 
-#-------------------------
+
 if __name__ == '__main__':
     app.run(debug=True)
